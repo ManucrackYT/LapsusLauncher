4a7fac9c058c02d690d68f1eda237bd83caaeaff
Manucrack YT <71397795+ManucrackYT@users.noreply.github.com>
2023-10-01 17:22:11 +0200

    LapsusLauncher

---
+++ app/assets/js/distromanager.js
@@ -0,0 +1,17 @@
+const { DistributionAPI } = require('helios-core/common')
+
+const ConfigManager = require('./configmanager')
+
+// Old WesterosCraft url.
+// exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://fr02.planethost.xyz:3002/distribution.json'
+
+const api = new DistributionAPI(
+    ConfigManager.getLauncherDirectory(),
+    null, // Injected forcefully by the preloader.
+    null, // Injected forcefully by the preloader.
+    exports.REMOTE_DISTRO_URL,
+    false
+)
+
+exports.DistroAPI = api
