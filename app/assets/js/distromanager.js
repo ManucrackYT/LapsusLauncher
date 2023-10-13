bcac96530437661955da6be9a9933a459f862e30
Manucrack YT <71397795+ManucrackYT@users.noreply.github.com>
2023-10-13 09:26:43 +0200

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Old WesterosCraft url.
 // exports.REMOTE_DISTRO_URL = 'http://mc.westeroscraft.com/WesterosCraftLauncher/distribution.json'
-exports.REMOTE_DISTRO_URL = 'http://fr02.planethost.xyz:3002/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://fr1.planethost.xyz:3005/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
