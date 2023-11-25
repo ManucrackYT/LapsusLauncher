207a2eb02c2e40dcffbdb97ca8bcd9ef44925b25
Manucrack YT <71397795+ManucrackYT@users.noreply.github.com>
2023-11-26 00:06:48 +0100

    Update distromanager.js

--- app/assets/js/distromanager.js
+++ app/assets/js/distromanager.js
@@ -4,7 +4,7 @@ const ConfigManager = require('./configmanager')
 
 // Listening on node 2
 
-exports.REMOTE_DISTRO_URL = 'http://fr02.planethost.xyz:3002/distribution.json'
+exports.REMOTE_DISTRO_URL = 'http://de02.planethost.xyz:3002/distribution.json'
 
 const api = new DistributionAPI(
     ConfigManager.getLauncherDirectory(),
